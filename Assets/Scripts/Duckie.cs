f339298b32affb3280d83f4349030253374356e1
Mike Disa <disa@sheridancollege.ca>
2026-02-03 10:01:21 -0500

    Tested for UI overlap

---
+++ Assets/Scripts/Duckie.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class Duckie : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        //was the mouse pressed this frame? && mouse not over ui
+        if(Mouse.current.leftButton.wasPressedThisFrame && !EventSystem.current.IsPointerOverGameObject())
+        {
+            //move to that position
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            transform.position = mousePos;
+        }
+        
+    }
+}
